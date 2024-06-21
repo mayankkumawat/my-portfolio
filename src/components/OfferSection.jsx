9d8cfbef18250f50f095805292ca244c0576cb81
Mayank Kumawat <mayankkumawat015@gmail.com>
2024-06-22 01:15:25 +0530

    fix - styles offer section

--- src/components/OfferSection.jsx
+++ src/components/OfferSection.jsx
@@ -1,68 +1,18 @@
 import React from 'react';
-import {
-  FaMobile,
-  FaFigma,
-  FaLaptopCode,
-  FaRegHandshake,
-} from 'react-icons/fa';
-import { FaBookTanakh } from 'react-icons/fa6';
+import { Constants } from '@/constants/constants';
 
 import '../styles/globals.css';
-
-const WORK_INFO = [
-  {
-    id: 1,
-    title: 'Mobile App Development',
-    icon: <FaMobile size={'2rem'} />,
-    description:
-      'Crafting intuitive and high-performing mobile applications for iOS and Android, tailored to meet user needs and business goals.',
-  },
-  {
-    id: 2,
-    icon: <FaLaptopCode size={'2rem'} />,
-    title: 'Web Development',
-    description:
-      'Building responsive and dynamic websites with cutting-edge technologies to enhance user experience and drive engagement.',
-  },
-  {
-    id: 3,
-    icon: <FaFigma size={'2rem'} />,
-    title: 'UI-UX Designer (Figma)',
-    description:
-      'Designing user-centric interfaces and seamless experiences in Figma, focusing on usability and aesthetic appeal.',
-  },
-  {
-    id: 4,
-    icon: <FaRegHandshake size={'2rem'} />,
-    title: 'App Consultant',
-    description:
-      'Providing expert guidance on app strategy, design, and development to optimize functionality and user satisfaction.',
-  },
-  {
-    id: 5,
-    icon: <FaBookTanakh size={'2rem'} />,
-    title: 'Research and Development',
-    description:
-      'Innovating and exploring new technologies to drive forward-thinking solutions and continuous improvement in software development.',
-  },
-  {
-    id: 6,
-    icon: <FaFigma size={'2rem'} />,
-    title: 'Project Management',
-    description:
-      'Leading projects from concept to completion, ensuring timely delivery, budget adherence, and alignment with client objectives.',
-  },
-];
 
 const OfferSection = () => {
   return (
     <div className="offer-container">
       <div className="title">
         <p>My Activities</p>
-        <div className="under-line"></div>
+        <div className="small-under-line" />
+        <div className="under-line" />
       </div>
       <div className="work-container">
-        {WORK_INFO.map(({ id, title, description, icon }) => (
+        {Constants.WORK_INFO.map(({ id, title, description, icon }) => (
           <div key={id} className="work-item">
             <div>{icon}</div>
             <div className="work-info">
--- src/constants/constants.tsx
+++ src/constants/constants.tsx
@@ -1,5 +1,59 @@
+import {
+  FaBusinessTime,
+  FaHandshakeSimple,
+  FaMagnifyingGlassChart,
+} from 'react-icons/fa6';
+import { CgFigma } from 'react-icons/cg';
+import { FaMobileAlt, FaLaptopCode } from 'react-icons/fa';
+
 const NAVBAR_LINKS = ['About me', 'Resume', 'Works', 'Contact'];
 
+const WORK_INFO = [
+  {
+    id: 1,
+    title: 'Mobile App Development',
+    icon: <FaMobileAlt color={'#0088ff'} size={'1.5rem'} />,
+    description:
+      'Crafting intuitive and high-performing mobile applications for iOS and Android, tailored to meet user needs and business goals.',
+  },
+  {
+    id: 2,
+    icon: <FaLaptopCode color={'#0088ff'} size={'1.5rem'} />,
+    title: 'Web Development',
+    description:
+      'Building responsive and dynamic websites with cutting-edge technologies to enhance user experience and drive engagement.',
+  },
+  {
+    id: 3,
+    icon: <CgFigma color={'#0088ff'} size={'1.5rem'} />,
+    title: 'UI-UX Designer (Figma)',
+    description:
+      'Designing user-centric interfaces and seamless experiences in Figma, focusing on usability and aesthetic appeal.',
+  },
+  {
+    id: 4,
+    icon: <FaHandshakeSimple color={'#0088ff'} size={'1.5rem'} />,
+    title: 'App Consultant',
+    description:
+      'Providing expert guidance on app strategy, design, and development to optimize functionality and user satisfaction.',
+  },
+  {
+    id: 5,
+    icon: <FaBusinessTime color={'#0088ff'} size={'1.5rem'} />,
+    title: 'Project Management',
+    description:
+      'Leading projects from concept to completion, ensuring timely delivery, budget adherence, and alignment with client objectives.',
+  },
+  {
+    id: 6,
+    icon: <FaMagnifyingGlassChart color={'#0088ff'} size={'1.5rem'} />,
+    title: 'Research & Development',
+    description:
+      'Innovating and exploring new technologies to drive forward-thinking solutions and continuous improvement in software development.',
+  },
+];
+
 export const Constants = {
+  WORK_INFO,
   NAVBAR_LINKS,
 };
